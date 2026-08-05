ea33fd8d9e6d457452c15b00e3b5019083418b6c
Hunter Alzate <hunteralzate@yahoo.com>
2026-08-04 17:58:17 -0700

    Update q1.py

--- tests/q1.py
+++ tests/q1.py
@@ -2,42 +2,38 @@ OK_FORMAT = True
 
 test = {
   'name': 'q1',
-  'points': 3,
+  'points': 2,
   'suites': [
     {
+      # Import both required libraries and set the seed before testing
+      'setup': r"""
+      >>> import random
+      >>> import math
+      >>> random.seed(42)
+      """,
       'cases': [
         {
           'code': r"""
-          >>> square(5)
-          25
+          >>> result_1 = sim_diff_integers(5,4,10000)
+          >>> # Checks if result_1 is within 0.01 of 1.82
+          >>> math.isclose(result_1, 0.23, abs_tol=1e-2)
+          True
           """,
           'hidden': False
         },
         {
           'code': r"""
-          >>> square(2.5)
-          6.25
-          """,
-          'hidden': False
-        },
-        {
-          'code': r"""
-          >>> square(-4)
-          16
-          """,
-          'hidden': True
-        },
-        {
-          'code': r"""
-          >>> square(0)
-          0
+          >>> result_2 = sim_diff_integers(20,4,10000)
+          >>> # Checks if the next random generation is within 0.01 of 0.98
+          >>> math.isclose(result_2, 0.98, abs_tol=1e-2)
+          True
           """,
           'hidden': True
         }
       ],
       'scored': True,
-      'setup': '',
       'teardown': '',
       'type': 'doctest'
     }
   ]
+}
